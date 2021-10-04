6cf891ea09fda1af314a5b342bbd45395576dfa3
regola-Tacens <regolas-tacens@outlook.fr>
2021-10-04 06:56:30 +0200

    ajout css sur input et submit button

--- src/components/Argonautes/index.js
+++ src/components/Argonautes/index.js
@@ -1,16 +1,25 @@
-import Argonaute from './Argonaute';
-import './styles.css'
+import Argonaute from "./Argonaute";
+import "./styles.css";
 
-const Argonautes = ({ argonautes,setTrigger }) => {
-  
+const Argonautes = ({ argonautes, setTrigger }) => {
   return (
     <>
-    <h2><span className="argonaute__index">{argonautes.length}</span> Membres d'équipage</h2>
-    <div className="argonautesList">
-      {argonautes.map((argonaute,index) => (
-        <Argonaute key={argonaute.id} argonaute={argonaute} index={index} setTrigger={setTrigger}/>
-      ))}
-    </div>
+      <h2>
+        <span className="argonaute__index">{argonautes.length}</span> Membres
+        d'équipage
+        {argonautes.length === 50 && <span> , le bateau est plein !</span>}
+      </h2>
+
+      <div className="argonautesList">
+        {argonautes.map((argonaute, index) => (
+          <Argonaute
+            key={argonaute.id}
+            argonaute={argonaute}
+            index={index}
+            setTrigger={setTrigger}
+          />
+        ))}
+      </div>
     </>
   );
 };
--- src/components/FormArgo/Form.js
+++ src/components/FormArgo/Form.js
@@ -1,11 +1,11 @@
 const Form = ({ argonaute, handleChange, handleSubmit }) => (
   <div className="formArgo">
     <h2>Ajouter un(e) Argonaute</h2>
-    <form>
-      <label>Nom de l&apos;Argonaute</label>
-      <input value={argonaute} onChange={handleChange} required />
-      <button type="submit" onClick={handleSubmit}>
-        send
+    <form className="argonaute__form">
+      <label className="argonaute__form__label">Nom de l&apos;Argonaute</label>
+      <input className="argonaute__form__input" value={argonaute} onChange={handleChange} required />
+      <button className="argonaute__form__submitBtn" type="submit" onClick={handleSubmit}>
+        +
       </button>
     </form>
   </div>
